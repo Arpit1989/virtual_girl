1c2ec7dcc8a61d01fe7498d552d2fc6b1efe010b
Arpit Kulshrestha <Arpit_Kulshrestha@external.mckinsey.com>
2015-06-24 03:42:30 +0530

    Added a check for undefined results

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::API
   def load_samantha
     begin
       response = Samantha.new params[:ques]
+      if (response.say.empty?)
+        response = { say: {answer: "Could Not find the answer, will learn more , shit happens!"}}
+      end
       render json: response.to_json
     rescue
       render json: { say: {answer:"error"} }
